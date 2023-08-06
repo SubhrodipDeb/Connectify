7aee5576a511202e776f8e3854496642e27cf2ed
SubhrodipDeb <77625763+SubhrodipDeb@users.noreply.github.com>
2023-08-06 16:11:56 +0530

    Add files via upload

---
+++ Server/Routes/chatRoute.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const { createChat,findChat,findUserChats} = require("../Controller/chatController")
+
+const router = express.Router();
+
+
+router.post("/",createChat);
+router.get("/:userId",findUserChats);
+router.get("/find/:firstId/:secondId",findChat);
+
+module.exports = router;
